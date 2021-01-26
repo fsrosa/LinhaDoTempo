df7117a3c1c6a45eb781359c1300ebad575d6871
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2021-01-25 22:34:42 -0300

    implementing routes

--- time-line/src/App.jsx
+++ time-line/src/App.jsx
@@ -1,9 +1,10 @@
+import React from 'react'
+import Routes from './routes'
+
 function App() {
   return (
     <div>
-      Hello, World! I'm Time Line App!
-
-      (I'm testing to show to Patrick)
+      <Routes />
     </div>
   );
 }
---
+++ time-line/src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+    return (
+        <div>
+            Home Page Time Line
+            <div>
+                Apenas um Teste de Deploy
+            </div>
+        </div>
+
+    )
+}
