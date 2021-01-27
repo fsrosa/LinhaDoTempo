3af78ae9a8a25b2df321768b88cc59e0e424c6fa
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2021-01-26 22:48:24 -0300

    implementing basic react-bootstrap

--- time-line/src/pages/Home/index.jsx
+++ time-line/src/pages/Home/index.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import './styles.css'
+
+import { Navbar, NavDropdown, Nav } from 'react-bootstrap'
+import linhaDoTempoLogo from '../../assets/logo-linhasdotempo.gif'
 
 export default function Home() {
     return (
-        <div>
-            Home Page Time Line
-            <div>
-                Apenas um Teste de Deploy
-            </div>
+        <div className="home-container">
+            <Navbar bg="light" expand="lg" className="main-header">
+                <Navbar.Brand href="#home">
+                    <img src={linhaDoTempoLogo} alt="" />
+                </Navbar.Brand>
+                <Nav.Link href="#home">Home</Nav.Link>
+                <NavDropdown title="Fabrício Rosa">
+                    <NavDropdown.Item href="#account">Minha Conta</NavDropdown.Item>
+                    <NavDropdown.Item href="#acontecimentos">Meus Acontecimentos</NavDropdown.Item>
+                    <NavDropdown.Item href="#linhas-do-tempo">Minhas Linhas do Tempo</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#logout">Sair</NavDropdown.Item>
+                </NavDropdown>
+            </Navbar>
         </div>
+
 
     )
 }
