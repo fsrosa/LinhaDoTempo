f103ef1c07c319509135c5890882ecda61ddccb3
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2021-01-27 22:30:06 -0300

    deploying

--- time-line/src/pages/Home/index.jsx
+++ time-line/src/pages/Home/index.jsx
@@ -9,8 +9,11 @@ import Menu from '@material-ui/core/Menu';
 import MenuIcon from '@material-ui/icons/Menu'
 import MenuItem from '@material-ui/core/MenuItem';
 import AccountCircle from '@material-ui/icons/AccountCircle'
-import logoIcon from '../../assets/logo-linhasdotempo.gif'
 import { makeStyles } from '@material-ui/core';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+
+
+import logoIcon from '../../assets/logo-linhasdotempo.gif'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -21,6 +24,9 @@ const useStyles = makeStyles((theme) => ({
     },
     title: {
         flexGrow: 1,
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
     },
     logo: {
         maxHeight: "5rem",
@@ -29,14 +35,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Home() {
     const classes = useStyles()
-    // const [auth, setAuth] = useState(true)
-    const auth = true
     const [anchorEl, setAnchorEl] = useState(null)
     const open = Boolean(anchorEl)
-
-    // const handleChange = (event) => {
-    //     setAuth(event.target.checked)
-    // }
 
     const handleMenu = (event) => {
         setAnchorEl(event.currentTarget)
@@ -48,9 +48,14 @@ export default function Home() {
 
     return (
         <div className={classes.root}>
-            <AppBar position="static">
+            <AppBar position="static" >
                 <Toolbar className="home-toolbar">
-                    <IconButton className={classes.menuButton} edge="start" color="primary" aria-label="menu">
+                    <IconButton
+                        className={classes.menuButton}
+                        edge="start"
+                        color="primary"
+                        aria-label="menu"
+                    >
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
@@ -59,41 +64,45 @@ export default function Home() {
                             src={logoIcon}
                             alt="timeline-logo"
                         ></img>
-                    </Typography>
-                    {auth && (
-                        <div className="home-user-icon">
-                            <IconButton
-                                aria-label="account of current user"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleMenu}
+                        <IconButton>
+                            <AddCircleOutlineIcon
                                 color="primary"
-                            >
-                                <h6>Fabrício Rosa</h6>
-                                <AccountCircle />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorEl}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'center',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'center',
-                                }}
-                                open={open}
-                                onClose={handleClose}
+                                fontSize="large"
+                            />
+                        </IconButton>
+                    </Typography>
+                    <div className="home-user-icon">
+                        <IconButton
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleMenu}
+                            color="primary"
+                        >
+                            <h6>Ana Santos</h6>
+                            <AccountCircle />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                            }}
+                            open={open}
+                            onClose={handleClose}
 
-                            >
-                                <MenuItem>Minha Conta</MenuItem>
-                                <MenuItem>Minhas Histórias</MenuItem>
-                                <MenuItem>Minhas Linhas do Tempo</MenuItem>
-                            </Menu>
-                        </div>
-                    )}
+                        >
+                            <MenuItem onClick={handleClose}>Minha Conta</MenuItem>
+                            <MenuItem onClick={handleClose}>Minhas Histórias</MenuItem>
+                            <MenuItem onClick={handleClose}>Minhas Linhas do Tempo</MenuItem>
+                        </Menu>
+                    </div>
                 </Toolbar>
             </AppBar>
         </div>
