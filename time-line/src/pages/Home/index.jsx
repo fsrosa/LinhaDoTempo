1af76783b220b72420446e0bb22959b10fdfd07f
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2021-01-28 21:54:53 -0300

    implementing time-line dialog - basic structure

--- time-line/src/pages/Home/index.jsx
+++ time-line/src/pages/Home/index.jsx
@@ -11,6 +11,14 @@ import MenuItem from '@material-ui/core/MenuItem';
 import AccountCircle from '@material-ui/icons/AccountCircle'
 import { makeStyles } from '@material-ui/core';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
 
 
 import logoIcon from '../../assets/logo-linhasdotempo.gif'
@@ -37,6 +45,7 @@ export default function Home() {
     const classes = useStyles()
     const [anchorElUser, setAnchorElUser] = useState(null)
     const [anchorElAdd, setAnchorElAdd] = useState(null)
+    const [openAddDialog, setOpenAddDialog] = useState(false);
     const openUser = Boolean(anchorElUser)
     const openAdd = Boolean(anchorElAdd)
 
@@ -54,6 +63,15 @@ export default function Home() {
 
     const handleCloseAdd = (event) => {
         setAnchorElAdd(null)
+    }
+
+    const handleClickOpenAddDialog = (event) => {
+        setAnchorElAdd(null)
+        setOpenAddDialog(true)
+    }
+
+    const handleCloseAddDialog = (event) => {
+        setOpenAddDialog(false)
     }
 
     return (
@@ -74,68 +92,101 @@ export default function Home() {
                             src={logoIcon}
                             alt="timeline-logo"
                         ></img>
-                        <IconButton
-                            aria-label="add action"
-                            aria-haspopup="true"
-                            aria-controls="menu-add"
-                            onClick={handleMenuAdd}
-                        >
-                            <AddCircleOutlineIcon
-                                color="primary"
-                                fontSize="large"
-                            />
-                        </IconButton>
-                        <Menu
-                            id="menu-add"
-                            anchorEl={anchorElAdd}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'right',
-                            }}
-                            open={openAdd}
-                            onClose={handleCloseAdd}
-                        >
-                            <MenuItem onClick={handleCloseAdd}>Adicionar História</MenuItem>
-                            <MenuItem onClick={handleCloseAdd}>Adicionar Linha do Tempo</MenuItem>
-                        </Menu>
+                        <div className="add-container">
+                            <IconButton
+                                aria-label="add action"
+                                aria-haspopup="true"
+                                aria-controls="menu-add"
+                                onClick={handleMenuAdd}
+                            >
+                                <AddCircleOutlineIcon
+                                    color="primary"
+                                    fontSize="large"
+                                />
+                            </IconButton>
+                            <Menu
+                                id="menu-add"
+                                anchorEl={anchorElAdd}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'right',
+                                }}
+                                transformOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'right',
+                                }}
+                                open={openAdd}
+                                onClose={handleCloseAdd}
+                            >
+                                <MenuItem onClick={handleCloseAdd}>Adicionar História</MenuItem>
+                                <MenuItem onClick={handleClickOpenAddDialog}>Adicionar Linha do Tempo</MenuItem>
+                            </Menu>
+                            <Dialog open={openAddDialog} onClose={handleCloseAddDialog} aria-labelledby="form-dialog-title">
+                                <DialogTitle id="form-dialog-title">Adicionar Linha do Tempo</DialogTitle>
+                                <DialogContent>
+                                    <TextField
+                                        autoFocus
+                                        label="Nome da Linha do Tempo"
+                                        type="text"
+                                        margin="normal"
+                                        fullWidth
+                                    />
+                                    <TextField
+                                        label="WebSite"
+                                        type="url"
+                                        margin="normal"
+                                        fullWidth
+                                    />
+                                    {/* <input accept="image/*" id="icon-button-file" type="file" />
+                                    <label htmlFor="icon-button-file">
+                                        <IconButton color="primary" aria-label="upload picture" component="span">
+                                            <PhotoCamera />
+                                        </IconButton>
+                                    </label> */}
+                                </DialogContent>
+                                    <DialogActions>
+                                        <Button onClick={handleCloseAddDialog} color="primary">
+                                            Cancel
+                                    </Button>
+                                        <Button onClick={handleCloseAddDialog} color="primary">
+                                            Subscribe
+                                    </Button>
+                                    </DialogActions>
+                            </Dialog>
+                        </div>
                     </Typography>
-                    <div className="home-user-icon">
-                        <IconButton
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleMenuUser}
-                            color="primary"
-                        >
-                            <h6>Ana Santos</h6>
-                            <AccountCircle />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={openUser}
-                            onClose={handleCloseUser}
+                        <div className="home-user-icon">
+                            <IconButton
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenuUser}
+                                color="primary"
+                            >
+                                <h6>Ana Santos</h6>
+                                <AccountCircle />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={openUser}
+                                onClose={handleCloseUser}
 
-                        >
-                            <MenuItem onClick={handleCloseUser}>Minha Conta</MenuItem>
-                            <MenuItem onClick={handleCloseUser}>Minhas Histórias</MenuItem>
-                            <MenuItem onClick={handleCloseUser}>Minhas Linhas do Tempo</MenuItem>
-                        </Menu>
-                    </div>
+                            >
+                                <MenuItem onClick={handleCloseUser}>Minha Conta</MenuItem>
+                                <MenuItem onClick={handleCloseUser}>Minhas Histórias</MenuItem>
+                                <MenuItem onClick={handleCloseUser}>Minhas Linhas do Tempo</MenuItem>
+                            </Menu>
+                        </div>
                 </Toolbar>
             </AppBar>
         </div>
