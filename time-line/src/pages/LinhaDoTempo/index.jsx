634eb123a79451f798202e17b62758f27f5f42f4
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2021-02-04 22:16:30 -0300

    creating structure to Time Line Page

--- time-line/src/pages/LinhaDoTempo/index.jsx
+++ time-line/src/pages/LinhaDoTempo/index.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 // import { useHistory } from 'react-router-dom';
 import api from '../../services/api'
-import useStyles from './useStyles'
+// import useStyles from './useStyles'
+
+import './styles.css'
 
 export default function TimeLines() {
-    const classes = useStyles()
+    // const classes = useStyles()
     // const history = useHistory()
 
     const [timeLines, setTimeLines] = useState([])
@@ -14,27 +16,76 @@ export default function TimeLines() {
             .then(response => {
                 setTimeLines(response.data)
             })
-    }, [])
+    }, [timeLines])
 
     return (
-        <div className={classes.root}>
-            <h1>Linhas do Tempo:</h1>
-            
-            <ul>
-                {timeLines.map(timelines => (
-                    <li key={timelines.id}>
+        <div className="root">
+            <h1>Todas as Linhas do Tempo:</h1>
+            <div className="timelines-cards">
+                <ul className="cards-container">
+                    {timeLines.map(timelines => (
+                        <li key={timelines.id}>
+                            <strong>Linha:</strong>
+                            <p>{timelines.timeLineName}</p>
+
+                            <strong>WebSite:</strong>
+                            <p>{timelines.website}</p>
+
+                            <strong>Resumo:</strong>
+                            <p>{timelines.resume}</p>
+                        </li>
+                    ))}
+                    <li className="line-card">
                         <strong>Linha:</strong>
-                        <p>{timelines.timeLineName}</p>
+                        <p>Minha Infância</p>
 
                         <strong>WebSite:</strong>
-                        <p>{timelines.website}</p>
+                        <p>www.fabricio-rosa.com.br</p>
 
                         <strong>Resumo:</strong>
-                        <p>{timelines.resume}</p>
+                        <p>Aqui em conto a história da minha infância.
+                        Como cresci, aprendi e me tornei
+                        um grande adulto!</p>
                     </li>
-                ))}
+                    <li className="line-card">
+                        <strong>Linha:</strong>
+                        <p>Minha Infância</p>
 
-            </ul>
+                        <strong>WebSite:</strong>
+                        <p>www.fabricio-rosa.com.br</p>
+
+                        <strong>Resumo:</strong>
+                        <p>Aqui em conto a história da minha infância.
+                        Como cresci, aprendi e me tornei
+                        um grande adulto!</p>
+                    </li>
+                    <li className="line-card">
+                        <strong>Linha:</strong>
+                        <p>Minha Infância</p>
+
+                        <strong>WebSite:</strong>
+                        <p>www.fabricio-rosa.com.br</p>
+
+                        <strong>Resumo:</strong>
+                        <p>Aqui em conto a história da minha infância.
+                        Como cresci, aprendi e me tornei
+                        um grande adulto!</p>
+                    </li>
+                    <li className="line-card">
+                        <strong>Linha:</strong>
+                        <p>Minha Infância</p>
+
+                        <strong>WebSite:</strong>
+                        <p>www.fabricio-rosa.com.br</p>
+
+                        <strong>Resumo:</strong>
+                        <p>Aqui em conto a história da minha infância.
+                        Como cresci, aprendi e me tornei
+                        um grande adulto!</p>
+                    </li>
+
+                </ul>
+            </div>
         </div>
     )
 }   
--- time-line/src/pages/LinhaDoTempo/useStyles.jsx
+++ time-line/src/pages/LinhaDoTempo/useStyles.jsx
@@ -2,7 +2,6 @@ import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles(() => ({
     root: {
-        backgroundColor: 'gray',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
