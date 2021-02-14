d61b8a23a788c4841bde0520d51d429e2701f923
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2021-02-14 19:46:10 -0300

    implementing header component and logon page

---
+++ time-line/src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+import './Header.css'
+import logoIcon from '../../assets/logo-linhasdotempo.gif'
+
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+function Header() {
+    return(
+        <div className="header-container">
+            <AppBar position="static" className="appbar">
+                <Toolbar className="toolbar">
+                    <Typography>
+                        <img src={logoIcon} className="logo-icon" alt="timeline-logo"/>
+                    </Typography>
+                    <Typography variant="h6" color="primary">
+                        Sobre
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Header
--- time-line/src/routes.jsx
+++ time-line/src/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import Logon from './pages/Logon'
 import Home from './pages/Home'
 import TimeLines from './pages/LinhaDoTempo'
 
@@ -8,8 +9,9 @@ export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Home}/>
+                <Route path="/" exact component={Logon}/>
                 <Route path="/time-lines" component={TimeLines}/>
+
             </Switch>
         </BrowserRouter>
     )
